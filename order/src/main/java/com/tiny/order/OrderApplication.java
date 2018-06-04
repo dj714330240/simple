697ca6a4c79b016220e3885eff4fcf6fa5d4cc86
714330240@qq.com <714330240@qq.com>
2018-06-04 19:13:41 +0800

    声明Eureka客户端

--- order/src/main/java/com/tiny/order/OrderApplication.java
+++ order/src/main/java/com/tiny/order/OrderApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/***
+ * 声明一个Eureka客户端
+ */
 @EnableEurekaClient
 @SpringBootApplication
 public class OrderApplication {
